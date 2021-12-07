e4456344b066b4841c521f91fe09a1afd0bd1102
Christian Fuentes <christiangfv@gmail.com>
2021-12-06 22:58:35 -0300

    App que captura y guarda el video en api

---
+++ blood_preassure_app/android/app/src/main/kotlin/com/example/head_pulse_track/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.head_pulse_track
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ blood_preassure_app/android/app/src/main/kotlin/com/example/head_pulse_track_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.head_pulse_track_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
